ff0261db034128850162608b6ec71e20528028db
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2025-12-04 20:38:29 +0000

    Pull out main logic into its own function.

--- cyberpunk_radio_extractor/__init__.py
+++ cyberpunk_radio_extractor/__init__.py
@@ -26,8 +26,72 @@ Extract Cyberpunk 2077 radios (and jingles) as MP3 files with album art.
 #  OR OTHER DEALINGS IN THE SOFTWARE.
 #
 
+# stdlib
+from os import PathLike
+
+# 3rd party
+import tqdm
+from cp2077_extractor.audio_data.radio_stations import Track, radio_jingle_ids, radio_stations
+from cp2077_extractor.redarchive_reader import REDArchive
+from domdf_python_tools.paths import PathPlus
+
+# this package
+from cyberpunk_radio_extractor.album_art import get_album_art
+from cyberpunk_radio_extractor.audio import extract_track
+
+__all__ = ["extract_radio_songs"]
+
 __author__: str = "Dominic Davis-Foster"
 __copyright__: str = "2025 Dominic Davis-Foster"
 __license__: str = "MIT License"
 __version__: str = "0.0.0"
 __email__: str = "dominic@davis-foster.co.uk"
+
+
+def extract_radio_songs(
+		install_dir: PathLike,
+		output_dir: PathLike,
+		jingles: bool = True,
+		verbose: bool = False,
+		) -> None:
+	"""
+	Extract Cyberpunk 2077 radios (and jingles) as MP3 files with album art.
+
+	:param jingles: Also extract jingles.
+	:param install_dir: Path to the Cyberpunk 2077 installation.
+	:param output_dir:
+	:param verbose:
+	"""
+
+	install_dir_p = PathPlus(install_dir)
+
+	output_dir_p = PathPlus(output_dir)
+	output_dir_p.maybe_make()
+
+	archive_file = install_dir_p / "archive/pc/content" / "audio_2_soundbanks.archive"
+	assert archive_file.is_file()
+
+	album_art_data = get_album_art(install_dir_p)
+	archive = REDArchive.load_archive(archive_file)
+	total_tracks = sum(len(sd) for sd in radio_stations.values())
+
+	with open(archive_file, "rb") as fp, tqdm.tqdm(total=total_tracks) as progbar:
+		for station, station_data in radio_stations.items():
+			if verbose:
+				progbar.write(f"===== {station} =====")
+
+			station_dir = output_dir_p / station
+			station_dir.maybe_make()
+
+			for track in station_data:
+				mp3_filename = station_dir / (track.filename_stub + ".mp3")
+				if verbose:
+					progbar.write(f"    {track.filename_stub} -> {mp3_filename}")
+				extract_track(track, station, mp3_filename, archive, fp, album_art_data.get(station))
+				progbar.update()
+
+			if jingles and station in radio_jingle_ids:
+				for wem_name in radio_jingle_ids[station]:
+					mp3_filename = station_dir / f"jingle_{wem_name}.mp3"
+					track = Track(station, "Jingle", wem_name)
+					extract_track(track, station, mp3_filename, archive, fp, album_art_data.get(station))
--- cyberpunk_radio_extractor/__main__.py
+++ cyberpunk_radio_extractor/__main__.py
@@ -32,7 +32,7 @@ from consolekit.options import flag_option, version_option
 from consolekit.versions import get_version_callback
 
 # this package
-from cyberpunk_radio_extractor import __version__
+from cyberpunk_radio_extractor import __version__, extract_radio_songs
 
 __all__ = ["main"]
 
@@ -55,54 +55,13 @@ def main(
 	Extract Cyberpunk 2077 radios (and jingles) as MP3 files with album art.
 	"""
 
-	# 3rd party
-	import tqdm
-	from cp2077_extractor.audio_data.radio_stations import Track, radio_jingle_ids, radio_stations
-	from cp2077_extractor.redarchive_reader import REDArchive
-	from domdf_python_tools.paths import PathPlus
-
-	# this package
-	from cyberpunk_radio_extractor.album_art import get_album_art
-	from cyberpunk_radio_extractor.audio import extract_track
-
 	if not install_dir:
 		# 3rd party
 		import dom_toml
 		config = dom_toml.load("config.toml")
-		install_dir_p = PathPlus(config["config"]["install_dir"])
+		install_dir = config["config"]["install_dir"]
 
-	archive_file = install_dir_p / "archive/pc/content" / "audio_2_soundbanks.archive"
-	assert archive_file.is_file()
-
-	output_dir_p = PathPlus(output_dir)
-	output_dir_p.maybe_make()
-
-	album_art_data = get_album_art(install_dir_p)
-
-	archive = REDArchive.load_archive(archive_file)
-
-	total_tracks = sum(len(sd) for sd in radio_stations.values())
-
-	with open(archive_file, "rb") as fp, tqdm.tqdm(total=total_tracks) as progbar:
-		for station, station_data in radio_stations.items():
-			if verbose:
-				progbar.write(f"===== {station} =====")
-
-			station_dir = output_dir_p / station
-			station_dir.maybe_make()
-
-			for track in station_data:
-				mp3_filename = station_dir / (track.filename_stub + ".mp3")
-				if verbose:
-					progbar.write(f"    {track.filename_stub} -> {mp3_filename}")
-				extract_track(track, station, mp3_filename, archive, fp, album_art_data.get(station))
-				progbar.update()
-
-			if jingles and station in radio_jingle_ids:
-				for wem_name in radio_jingle_ids[station]:
-					mp3_filename = station_dir / f"jingle_{wem_name}.mp3"
-					track = Track(station, "Jingle", wem_name)
-					extract_track(track, station, mp3_filename, archive, fp, album_art_data.get(station))
+	extract_radio_songs(install_dir, output_dir, jingles=jingles, verbose=verbose)
 
 
 if __name__ == "__main__":
